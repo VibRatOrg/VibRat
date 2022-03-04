751c8e02a6e53feb784b33eccebea0e42663b245
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2022-03-04 20:37:55 +0530

    ⚡Update: Changes for Production

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,28 @@ import { readFileSync } from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+let serverOptions = {};
+if (process.env.NODE_ENV === "development") {
+  serverOptions = {
+    server: {
+      port: 443,
+      https: {
+        key: readFileSync(
+          process.env.HTTPS_CERTIFICATE_KEY_FILENAME_PATH,
+          "ascii"
+        ),
+        cert: readFileSync(
+          process.env.HTTPS_CERTIFICATE_FILENAME_PATH,
+          "ascii"
+        ),
+      }
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    port: 443,
-    https: {
-      key: readFileSync(
-        process.env.HTTPS_CERTIFICATE_KEY_FILENAME_PATH,
-        "ascii"
-      ),
-      cert: readFileSync(
-        process.env.HTTPS_CERTIFICATE_FILENAME_PATH,
-        "ascii"
-      ),
-    }
-  },
+  ...serverOptions,
 })
